662589c256ee53b07077fd6626e70bf011a552ac
Genji <chaos.nonlinear@gmail.com>
2018-11-15 21:46:44 +0900

    Initial Commit

---
+++ Tsuboyaki/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tsuboyaki
+//
+//  Created by Genji on 2018/11/15.
+//  Copyright © 2018 Genji App. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
---
+++ TsuboyakiTests/TsuboyakiTests.swift
@@ -0,0 +1,34 @@
+//
+//  TsuboyakiTests.swift
+//  TsuboyakiTests
+//
+//  Created by Genji on 2018/11/15.
+//  Copyright © 2018 Genji App. All rights reserved.
+//
+
+import XCTest
+@testable import Tsuboyaki
+
+class TsuboyakiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
