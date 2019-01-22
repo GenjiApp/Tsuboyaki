0729bfb1bdf6d2fa40b5656f5314ad228480ad2d
Genji <chaos.nonlinear@gmail.com>
2019-01-22 14:55:32 +0900

    change tweet compose textarea's height

--- Tsuboyaki/CustomStyle.js
+++ Tsuboyaki/CustomStyle.js
@@ -3,4 +3,5 @@ styleElement.textContent = 'div[data-testid="sidebarColumn"] { display: none !im
 styleElement.textContent += 'div[data-testid="primaryColumn"] { max-width: 100% !important; }';
 styleElement.textContent += 'main > div:first-child > div { max-width: 100% !important; }';
 styleElement.textContent += 'div[data-testid="primaryColumn"] > div > div { max-width: 100% !important; }';
+styleElement.textContent += 'textarea { height: 30vh !important; min-height: 120px !important; }'
 document.documentElement.appendChild(styleElement);
